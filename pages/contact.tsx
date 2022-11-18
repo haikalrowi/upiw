8076e57e34b65e7a4e3d2f48cdf3f09b2cf0392a
Haikal Rowi <haikalbintang27@gmail.com>
2022-11-18 22:10:21 +0700

    modify 'pages\contact.tsx'
    ~ modify 'pages\contact.tsx'
    ~ modify 'pages\index.tsx'
    ~ modify 'styles\globals.css'

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,10 +4,23 @@ import HeaderNav from "./components/headerNav";
 
 export default function () {
   return (
-    <div style={{ margin: 'auto 1rem' }}>
+    <>
       <Head><title>Haikal Rowi &ndash; Kontak</title></Head>
       <HeaderNav name="Kontak" />
-      <div>Contact</div>
-    </div>
+      <table>
+        <tbody>
+          <tr>
+            <td>Email</td>
+            <td>:</td>
+            <td><a href="mailto:haikalbintang27@gmail.com">haikalbintang27@gmail.com</a></td>
+          </tr>
+          <tr>
+            <td>WhatsApp</td>
+            <td>:</td>
+            <td><a href="https://wa.me/6282257799522">wa.me/6282257799522</a></td>
+          </tr>
+        </tbody>
+      </table>
+    </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import HeaderNav from "./components/headerNav";
 
 export default function Home() {
   return (
-    <div style={{ margin: '0px 1rem' }}>
+    <>
       <Head><title>Haikal Rowi &ndash; Portofolio</title></Head>
       <HeaderNav name='Haikal Rowi' />
       <div style={{ textAlign: 'center' }}>
@@ -14,14 +14,13 @@ export default function Home() {
           src={profilePicture}
           alt='Foto Haikal Rowi'
           loading="lazy"
-          height={256}
-          style={{ borderRadius: '50%' }}
+          style={{ borderRadius: '50%', maxWidth: '12rem', height: 'auto' }}
         />
       </div>
       <article>
         <p>Halo, saya seorang mahasiswa STIKOM PGRI Banyuwangi, jurusan S1 Teknik Informatika. Saya suka bermain dengan server.</p>
-        <p>Portofolio ini dibuat menggunakan <a href="https://nextjs.org/" target={'_blank'}>Next.js</a></p>
+        <p>Portofolio ini dibuat menggunakan <a href="https://nextjs.org/" target={'_blank'}>Next.js</a>,</p>
       </article>
-    </div>
+    </>
   )
 }
